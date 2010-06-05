20270e9b8ca60d16663add124aea23d5b92c4237
Daniel Cukier <danicuki@gmail.com>
2010-06-05 18:17:34 -0300

    applied layout to fact list and show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+	def select_menu path
+		return "selected_menu" if request.path == path
+	end
 end
