9f2c7f0479c6daaeeb90a2f5d08821aaea56300a
Daniel Cukier <danicuki@gmail.com>
2010-06-05 18:57:14 -0300

    cut description when it is too large

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 	def select_menu path
-		return "selected_menu" if request.path == path
+		return "selected_menu" if path.include? request.path
 	end
 end
--- app/models/fact.rb
+++ app/models/fact.rb
@@ -1,3 +1,8 @@
 class Fact < ActiveRecord::Base
 	has_many :metadatas
+	
+	def small_description
+		return description if description.size <= 20
+		"#{description[0..19]}..."
+	end
 end
