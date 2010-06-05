a62ec1e09dfcb0b6e2de11fbedf5ecbfd0f4dbba
Daniel Cukier <danicuki@gmail.com>
2010-06-05 18:01:26 -0300

    no index home and logs in gitignore

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ ActionController::Routing::Routes.draw do |map|
 		fact.resources :metadatas
   end
 
+	map.root :facts
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
