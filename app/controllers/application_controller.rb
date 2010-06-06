a9d81f103d2f5b0fd64e605e160ba4f9c49737a2
Daniel Cukier <danicuki@gmail.com>
2010-06-06 15:43:39 -0300

    about page and analytics code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
-	
 	before_filter :set_language
 	
 	def set_language
--- test/unit/hashtag_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class HashtagTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
-end
