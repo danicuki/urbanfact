4f96c7248a9ef2514016fa3ddfc2e57c3da8bb1e
Chester <cd@pobox.com>
2010-06-06 11:51:10 -0300

    Photos on map mouseover (with title on tooltip and link to the fact)

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,4 +1,5 @@
 var map;
+var infowindows = [];
 
 function load_map(){
     city_center = new google.maps.LatLng(-23.548153, -46.633101);
@@ -11,23 +12,31 @@ function load_map(){
     for (i in markers) {
         var m = markers[i];
         if (m && m.lat) {
-            add_marker(m.lat, m.lng, m.description, m.url, m.kind);
+            add_marker(m.lat, m.lng, m.description, m.url, m.kind, m.image_url);
         }
     }
 }
 
-function add_marker(lat, lng, title, url, kind){
+function add_marker(lat, lng, title, url, kind, photo){
     var latlng = new google.maps.LatLng(lat, lng);
+    var infowindow = new google.maps.InfoWindow({
+        content: '<div class="div_map_infowindow"><a href="'+url+'" alt="'+title+'" title="'+title+'"><img src="' + photo + '" class="img_map_scaled"></a></div>'
+    });
+    infowindows.push(infowindow);
     var marker = new google.maps.Marker({
         position: latlng,
         map: map,
         icon: "/images/marker_" + kind + ".png",
         title: title
     });
-    google.maps.event.addListener(marker, 'click', function(event){
-        window.location.href = url;
+    google.maps.event.addListener(marker, 'mouseover', function(event){
+        for (i in infowindows) {
+            infowindows[i].close();
+        }
+        infowindow.open(map, marker);
     });
 }
+
 
 function adjust_map_height(){
     n = $(document).height() - 125;
